8cb32ee7d04352f3eb97873b391857a336636276
fatemeh najafi <f.najafimoghadam@gmail.com>
2021-09-20 00:43:36 +0430

    fix: GetAllDevicesChildrenEndpoint syntax

--- Sources/FNMNetwork/EndPoints/getAllDevicesChildrenEndpoint.swift
+++ Sources/FNMNetwork/EndPoints/getAllDevicesChildrenEndpoint.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  GetAllDevicesChildrenEndpoint.swift
 //  
 //
 //  Created by Fatemeh Najafi on 6/28/1400 AP.
@@ -7,9 +7,9 @@
 import UIKit
 
 extension WebServiceManager {
-    public func getAllDevicesChildren(childId: String, page: Int, limit: Int  success: @escaping (DeviceModel) -> (), failure: @escaping (ErrorModel?, Error?) -> ()) -> URLSessionTask? {
+    public func getAllDevicesChildren(childId: String, page: Int, limit: Int, success: @escaping (DeviceModel) -> (), failure: @escaping (ErrorModel?, Error?) -> ()) -> URLSessionTask? {
         
-        return resumeDataTask(with: .getAllDevicesChildren(childId: childId, page: page, limit: limit), success: success, failure: failure).task
+        return resumeDataTask(with: .getAllDevicesChildren(childId: childId, page: page, limit: String(limit)), success: success, failure: failure).task
     }
     
 
